0026afc57884ba0be22f3dd77e610e3ae8aa1aff
Viktor Shavarin <viktor.shavarin@ocado.com>
2022-04-06 14:01:07 +0200

    removed redundant code

--- app/src/main/java/com/example/mymoviedb/MainApp.kt
+++ app/src/main/java/com/example/mymoviedb/MainApp.kt
@@ -3,21 +3,13 @@ package com.example.mymoviedb
 import AppComponent
 import DaggerAppComponent
 import android.app.Application
-import android.content.Context
 
 class MainApp : Application() {
 
     lateinit var appComponent: AppComponent
-        private set
 
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.create()
     }
 }
-
-val Context.appComponent: AppComponent
-    get() = when (this) {
-        is MainApp -> appComponent
-        else -> applicationContext.appComponent
-    }
